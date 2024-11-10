f997563ea7a8d9ad0044ec4b9e07124298dcc7be
norarcasey <git@stackblitz.com>
2024-11-09 20:45:24 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Puzzle } from './Puzzle';
+
+function App() {
+  return (
+    <div>
+      <Puzzle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Puzzle.tsx
@@ -0,0 +1,66 @@
+import Crossword from '@jaredreisinger/react-crossword';
+
+const data = {
+  across: {
+    1: {
+      clue: 'Anatomical bag',
+      answer: 'SAC',
+      row: 0,
+      col: 0,
+    },
+    4: {
+      clue: 'Gorgeous',
+      answer: 'NORA',
+      row: 1,
+      col: 0,
+    },
+    6: {
+      clue: 'Hot adult device?',
+      answer: 'IRON',
+      row: 2,
+      col: 0,
+    },
+    7: {
+      clue: 'Possible result of combat fatigue',
+      answer: 'PTSD',
+      row: 3,
+      col: 0,
+    },
+    8: {
+      clue: 'A stuborn person',
+      answer: 'ASS',
+      row: 4,
+      col: 1,
+    },
+  },
+  down: {
+    1: {
+      clue: 'Sound scissors make',
+      answer: 'SNIP',
+      row: 0,
+      col: 0,
+    },
+    2: {
+      clue: 'Large artery',
+      answer: 'AORTA',
+      row: 0,
+      col: 1,
+    },
+    3: {
+      clue: "Jesus's wood?",
+      answer: 'CROSS',
+      row: 0,
+      col: 2,
+    },
+    5: {
+      clue: '"No ifs ______ or buts"',
+      answer: 'ANDY',
+      row: 1,
+      col: 3,
+    },
+  },
+};
+
+export function Puzzle() {
+  return <Crossword data={data} />;
+}
